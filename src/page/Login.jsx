738dc12788d77cd6b419a9f49cff2d289abe5cee
mjwoon <jwmoon@hanyang.ac.kr>
2024-11-22 03:39:39 +0900

    change: change axios

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import "axios";
 import * as dotenv from "dotenv";
 
 dotenv.config();
--- src/page/Signup.jsx
+++ src/page/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+import "axios";
 import * as dotenv from "dotenv";
 
 dotenv.config();
